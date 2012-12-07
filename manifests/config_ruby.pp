cd087366f9fdaa8f11966728577021e2f8659062
Kristof Keppens <kristof@inuits.be>
2012-12-07 17:14:06 +0100

    remove remaining bugs to do a full bbb install

--- manifests/config_ruby.pp
+++ manifests/config_ruby.pp
@@ -6,8 +6,15 @@ class bigbluebutton::config_ruby {
     ppa    => 'ppa';
   }
 
-  package {"ruby1.9.2":
-    ensure => installed;
+  package {
+    "ruby1.9.2":
+      ensure => installed;
+    "rubygems1.9.2":
+      ensure => installed;
+    "make":
+      ensure => installed;
+    "ruby1.9.2-dev":
+      ensure => installed;
   }
 
 }
