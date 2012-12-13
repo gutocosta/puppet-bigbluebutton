a144036a691105812fc01057a9cdce57a4bb29b3
Kristof Keppens <kristof@inuits.be>
2012-12-13 16:25:33 +0100

    add config.xml configuration

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,37 @@
+# == Class: bigbluebutton::config
+#
+# This class manages the configuration of bigbluebutton
+#
+
 class bigbluebutton::config {
 
-  Exec {
-    require => Package['bigbluebutton']
-  }
+  require bigbluebutton::params
+
+  $help_url = $bigbluebutton::params::help_url
+  $enable_skin = $bigbluebutton::params::enable_skin
+  $theme_skin = $bigbluebutton::params::theme_skin
+  $translation_on = $bigbluebutton::params::translation_on
+  $translation_enabled = $bigbluebutton::params::translation_enabled
+  $private_chat = $bigbluebutton::params::private_chat
+  $allow_kick_user = $bigbluebutton::params::allow_kick_user
+  $phone_auto_join = $bigbluebutton::params::phone_auto_join
+  $phone_skip_check = $bigbluebutton::params::phone_skip_check
+  $video_quality = $bigbluebutton::params::video_quality
+  $presenter_share_only = $bigbluebutton::params::presenter_share_only
+  $resolutions = $bigbluebutton::params::resolutions
+  $cam_mode_fps = $bigbluebutton::params::cam_mode_fps
+  $cam_quality_bandwith = $bigbluebutton::params::cam_quality_bandwith
+  $cam_quality_picture = $bigbluebutton::params::cam_quality_picture
+  $enable_h264 = $bigbluebutton::params::enable_h264
 
   exec {
     'sethostname':
-      command => "/usr/local/bin/bbb-conf --setip $fqdn",
+      command => "/usr/local/bin/bbb-conf --setip $i{::fqdn}",
       unless  => '/usr/local/bin/bbb-conf --check';
     'setsalt':
-      command => "/usr/local/bin/bbb-conf --salt $bigbluebutton::salt",
+      command => "/usr/local/bin/bbb-conf --salt ${bigbluebutton::params::salt}",
       notify  => Exec['restartbbb'],
-      unless  => "/usr/local/bin/bbb-conf --salt | /bin/grep -q $bigbluebutton::salt";
+      unless  => "/usr/local/bin/bbb-conf --salt | /bin/grep -q ${bigbluebutton::params::salt}";
     'cleanbbb':
       command     => '/usr/local/bin/bbb-conf --clean',
       refreshonly => true;
@@ -26,10 +46,17 @@ class bigbluebutton::config {
       refreshonly => true;
   }
 
-  file {
-    '/etc/nginx/sites-available/bigbluebutton':
-      content => template("bigbluebutton/bigbluebutton-nginx.erb"),
-      notify  => Exec['restartbbb'],
-      require => Package['bigbluebutton'];
+  file { '/etc/nginx/sites-available/bigbluebutton':
+    ensure    => file,
+    content => template('bigbluebutton/bigbluebutton-nginx.erb'),
+    notify  => Exec['restartbbb'],
+    require => Package['bigbluebutton'];
+  }
+
+  file { '/var/www/bigbluebutton/client/conf/config.xml':
+    ensure  => file,
+    content => template('bigbluebutton/config.xml.erb'),
+    notify  => Exec['restartbbb'],
+    require => Package['bigbluebutton'];
   }
 }
