5650c640f0c0e7ca3809958e0688bdf4d25ae8fa
Kristof Keppens <kristof@inuits.be>
2013-01-09 09:29:29 +0100

    add make again because god gem cannot be installed without it

--- manifests/config_ruby.pp
+++ manifests/config_ruby.pp
@@ -7,6 +7,8 @@ class bigbluebutton::config_ruby {
       ensure => installed;
     'ruby1.9.2-dev':
       ensure => installed;
+    'make':
+      ensure => installed;
     'libopenssl-ruby1.9.2':
       ensure => installed;
   }
