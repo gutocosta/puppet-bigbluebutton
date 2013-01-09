d8b898c596f9f02527dd7c108a96adf8742376e0
Kristof Keppens <kristof@inuits.be>
2013-01-09 11:36:21 +0100

    add new config files and inherit from params to fill the templates with correct values

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@
 # This class manages the configuration of bigbluebutton
 #
 
-class bigbluebutton::config {
+class bigbluebutton::config () inherits bigbluebutton::params {
 
   exec {
     'restartbbb':
@@ -27,6 +27,20 @@ class bigbluebutton::config {
   file { '/var/www/bigbluebutton/client/conf/config.xml':
     ensure  => present,
     content => template('bigbluebutton/config.xml.erb'),
+    notify  => Exec['restartbbb'],
+    require => Package['bigbluebutton'];
+  }
+
+  file { '/var/lib/tomcat6/webapps/bigbluebutton/WEB-INF/classes/bigbluebutton.properties':
+    ensure  => present,
+    content => template('bigbluebutton/bigbluebutton.properties.erb'),
+    notify  => Exec['restartbbb'],
+    require => Package['bigbluebutton'];
+  }
+
+  file { '/usr/local/bigbluebutton/core/scripts/slides.yml':
+    ensure  => present,
+    content => template('bigbluebutton/slides.yml.erb',
     notify  => Exec['restartbbb'],
     require => Package['bigbluebutton'];
   }
