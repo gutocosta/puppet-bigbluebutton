4440360212a7826de467079139314e770b64aeee
Augusto da Silva Costa <augusto.costa@instruct.com.br>
2019-07-19 18:41:45 -0300

    Formatação do codigo

--- spec/bigbluebutton_spec.rb
+++ spec/bigbluebutton_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 describe 'bigbluebutton' do
-    context "on a Ubuntu OS" do
-      let :facts do
-        {
-          :kernel                 => 'Linux',
-          :osfamily               => 'Debian',
-          :operatingsystem        => 'Ubuntu',
-          :operatingsystemrelease => '16.04',
-        }
-      end
-      it { is.expected_to contain_class('bigbluebutton::pre_install') }
-      it { is.expected_to contain_class('bigbluebutton::install') }
-      it { is.expected_to contain_class('bigbluebutton::repos') }
-      it { is.expected_to contain_class('bigbluebutton::params') }
-      it { is.expected_to contain_class('bigbluebutton::config') }
-      it { is.expected_to contain_class('bigbluebutton::service') }
+  context 'on a Ubuntu OS' do
+    let :facts do
+
+      {
+        kernel:                 'Linux',
+        osfamily:               'Debian',
+        operatingsystem:        'Ubuntu',
+        operatingsystemrelease: '16.04',
+      }
     end
+    it { is.expected_to contain_class('bigbluebutton::pre_install') }
+    it { is.expected_to contain_class('bigbluebutton::install') }
+    it { is.expected_to contain_class('bigbluebutton::repos') }
+    it { is.expected_to contain_class('bigbluebutton::params') }
+    it { is.expected_to contain_class('bigbluebutton::config') }
+    it { is.expected_to contain_class('bigbluebutton::service') }
+  end
 end
